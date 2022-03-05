55a580635cfbc79bb56cffa04bdffe009a566343
varunsahu29 <varun.sahu.che19@itbhu.ac.in>
2022-03-05 21:58:22 +0530

    app.js
    Signed-off-by: varunsahu29 <varun.sahu.che19@itbhu.ac.in>

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ $.ajax({
   tasks = p["tasks"];
   employ_table = $("#emp_body");
   tasks = tasks.splice(0, 10);
-  "id": "4557",
-  "message": "Hi Shobha any comment.",
-  "assigned_to": "3",
-  "assigned_name": "Prabhat",
-  "created_on": "2022-01-06 11:20:05",
-  "due_date": "2022-01-06 10:20:00",
-  "priority": "3"
   for (task of tasks) {
     employ_table.after(`          <tr>
     <td>${task["id"]}</td>
