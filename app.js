4e69711cb8b89f4e13f2ba45c0ef14a7253cfb01
varunsahu29 <varun.sahu.che19@itbhu.ac.in>
2022-03-05 21:55:09 +0530

    app.js
    Signed-off-by: varunsahu29 <varun.sahu.che19@itbhu.ac.in>

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ $.ajax({
   headers: { AuthToken: "UrM4YHgb1FcqEf1tuKwmAMMX5MxFZ12a" },
 }).done((p) => {
   tasks = p["tasks"];
-  employ_table = $("#employee");
-  tasks.splice(0, 10);
-  employ_table.after(`          <tr>
-  <td>4543</td>
-  <td>goood</td>
-  <td>3</td>
-  <td>Prabhat</td>
-  <td>2022-01-06 08:43:40</td>
-  <td>2022-01-06 08:23:23</td>
-  <td>3</td>
-</tr>`);
+  employ_table = $("#emp_body");
+  tasks = tasks.splice(0, 10);
+  "id": "4557",
+  "message": "Hi Shobha any comment.",
+  "assigned_to": "3",
+  "assigned_name": "Prabhat",
+  "created_on": "2022-01-06 11:20:05",
+  "due_date": "2022-01-06 10:20:00",
+  "priority": "3"
+  for (task of tasks) {
+    employ_table.after(`          <tr>
+    <td>${task["id"]}</td>
+    <td>${task["message"]}</td>
+    <td>${task["assigned_to"]}</td>
+    <td>${task["assigned_name"]}</td>
+    <td>${task["created_on"]}</td>
+    <td>${task["due_date"]}</td>
+    <td>${task["priority"]}</td>
+  </tr>`);
+  }
 });
