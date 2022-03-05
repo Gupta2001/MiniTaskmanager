43c3bb237183e754dbe8f107aa38f6d2866c9054
varunsahu29 <varun.sahu.che19@itbhu.ac.in>
2022-03-05 14:50:13 +0530

    app.js modified
    Signed-off-by: varunsahu29 <varun.sahu.che19@itbhu.ac.in>

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 function list_task() {
-  $("#myTable tr:last").after(`
-
-    
-    `);
+  $.ajax({
+    url: "https://devza.com/tests/tasks/list",
+    headers: { AuthToken: "UrM4YHgb1FcqEf1tuKwmAMMX5MxFZ12a" },
+  }).done((p) => {
+    tasks = p["tasks"];
+    employ_table = $("#employee");
+  });
 }
